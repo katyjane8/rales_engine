452999570ceb17148f738098e69aebd118ff1163
katyjane8 <katy.jane8@gmail.com>
2018-01-23 14:46:26 -0700

    testing for record endpoints merchants and transactions pass

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
+      namespace :merchants do
+        get "/find", to: "search#show"
+        get "/find_all", to: "search#index"
+        get "/random", to: "random#show"
+      end
     end
   end
-
 end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,3 +1,9 @@
 RSpec.configure do |config|
+  module FactoryBot::Syntax::Methods
+    def config.params_for(model)
+      
+    end
+  end
+
   config.include FactoryBot::Syntax::Methods
 end
