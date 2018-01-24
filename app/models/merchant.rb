bb7645f68c8440102736af30c69dffabcc27ac2c
Josi McClellan <josiahmcclellan07@gmail.com>
2018-01-24 16:39:25 -0700

    add logic for merchants with most revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,6 +2,20 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
   has_many :transactions, through: :invoices
-  
+  has_many :invoice_items, through: :invoices
+
   validates_presence_of :name
+
+  def self.most_revenue(top_x)
+    select('
+      merchants.*,
+      sum(invoice_items.quantity * invoice_items.unit_price) AS revenue
+    ')
+      .joins(invoices: [:transactions, :invoice_items])
+      .where('transactions.result': :success)
+      .group(:id)
+      .order('sum(invoice_items.quantity * invoice_items.unit_price) DESC')
+      .limit(top_x)
+  end
+
 end
