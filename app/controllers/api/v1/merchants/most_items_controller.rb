f6f25aeb7bec8610072a283f17f8cc3d1e47995f
katyjane8 <katy.jane8@gmail.com>
2018-01-25 16:49:14 -0700

    merchant bi passing spec harness

--- app/controllers/api/v1/merchants/most_items_controller.rb
+++ app/controllers/api/v1/merchants/most_items_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Merchants::MostItemsController < ApplicationController
 
   def index
-    render json: Merchant.total_items
+    render json: Merchant.total_items(params[:quantity])
   end
 end
--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,14 +1,8 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
   def show
-    merch_rev = Merchant.find_by(rev_params)
+    merch_rev = Merchant.find(params[:merchant_id])
     rev = merch_rev.total_revenue.to_s.insert(-3, '.')
     render json: { "revenue" => rev }
   end
-
-  private
-
-    def rev_params
-      params.permit(Merchant.attribute_names)
-    end
 end
