679d06e9c8ba3260fb2c84c3883ab5e03177684b
Josi McClellan <josiahmcclellan07@gmail.com>
2018-01-25 14:16:36 -0700

    refactor routes
    Add methods to make simple but sprawling routes over many controllers more managable

--- config/routes.rb
+++ config/routes.rb
@@ -1,68 +1,65 @@
-Rails.application.routes.draw do
-  namespace :api, except: [:new, :edit], as: nil do
-    namespace :v1, as: nil do
+require_relative 'routes_dsl_expansion'
 
-      %i{ merchants customers items invoices invoice_items transactions}.each do |model|
-        namespace model do
+Rails.application.routes.draw do
+  singleton_class.include(RoutesDslExpansion)
+  namespace :api do
+    namespace :v1 do
+
+      collection_of :merchants do
+        indexes :most_items, :most_revenue
+        shows :revenue
+      end
+
+      record_of :merchant do
+        has_many :items, :invoices
+        indexes :customers_with_pending_invoices
+        shows :favorite_customer, :revenue
+      end
+
+      collection_of :items do
+        indexes :most_items, :most_revenue
+      end
+
+      record_of :items do
+        belongs_to :merchant
+        has_many :invoice_items
+        shows :best_day
+      end
+
+      record_of :invoices do
+        belongs_to :customer, :merchant
+        has_many :transactions, :items, :invoice_items
+      end
+
+      record_of :invoice_item do
+        belongs_to :item, :invoice
+      end
+
+      record_of :transaction do
+        belongs_to :invoice
+      end
+
+      record_of :customer do
+        has_many :invoices, :transactions
+        shows :favorite_merchant
+      end
+
+      %i{
+        merchant
+        customer
+        invoice
+        item
+        invoice_item
+        transaction
+      }.each do |model|
+        record_of model do
           get :find, to: "search#show"
           get :find_all, to: "search#index"
-          get :random, to: "random#show"
+          shows :random
         end
-      end
 
-      namespace :merchants do
-        get :most_items, to: 'most_items#index'
-        get :most_revenue, to: 'most_revenue#index'
-      end
-
-      namespace :items do
-        get :most_items, to: 'most_items#index'
-        get :most_revenue, to: 'most_revenue#index'
-      end
-
-      resources :merchants do
-        scope module: :merchants do
-          resources :items, :invoices, only: :index
-          get :revenue, to: 'revenue#show'
-          get :customers_with_pending_invoices,
-            to: 'customers_with_pending_invoices#index'
-          get :favorite_customer, to: 'favorite_customer#show'
-
-        end
-      end
-
-      resources :invoices do
-        scope module: :invoices do
-          resource :customer, :merchant, only: :show
-          resources :transactions, :items, :invoice_items, only: :index
-        end
-      end
-
-      resources :invoice_items do
-        scope module: :invoice_items do
-          resource :item, :invoice, only: :show
-        end
-      end
-
-      resources :items do
-        scope module: :items do
-          resource :merchant, only: :show
-          resources :invoice_items, only: :index
-          get :best_day, to: 'best_day#show'
-        end
-      end
-
-      resources :transactions do
-        scope module: :transactions do
-          resource :invoice, only: :show
-        end
-      end
-
-      resources :customers do
-        scope module: :customers do
-          resources :invoices, :transactions, only: :index
-          get :favorite_merchant, to: 'favorite_merchant#show'
-        end
+        # keep this at the end
+        resources model, only: [:show, :index]
       end
 
     end
