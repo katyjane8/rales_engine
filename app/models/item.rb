28910ffccedc3f37c81e78c9636df44a8163b609
Josi McClellan <josiahmcclellan07@gmail.com>
2018-01-25 02:15:41 -0700

    add logic for items with most revenue

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,14 @@ class Item < ApplicationRecord
 
   validates_presence_of :name, :description, :unit_price
 
+  def self.most_revenue(top_x)
+    select('items.*,
+      sum(invoice_items.quantity * invoice_items.unit_price) AS revenue')
+    .joins(invoice_items: { invoice: :transactions })
+    .where('transactions.result': :success)
+    .group(:id)
+    .order('sum(invoice_items.quantity * invoice_items.unit_price) DESC')
+    .limit(top_x)
+  end
+
 end
