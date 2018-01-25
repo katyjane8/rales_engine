899fb46538aaa65ddcdad302bcd027d2c78b4020
katyjane8 <katy.jane8@gmail.com>
2018-01-25 14:38:59 -0700

    customer fave merchant passing spec harness

--- app/controllers/api/v1/customers/favorite_merchant_controller.rb
+++ app/controllers/api/v1/customers/favorite_merchant_controller.rb
@@ -1,13 +1,6 @@
 class Api::V1::Customers::FavoriteMerchantController < ApplicationController
 
   def show
-    fav_merch = Customer.find_by(customer_params)
+    fav_merch = Customer.find(params[:customer_id])
     render json: fav_merch.favorite_merchant
   end
-
-  private
-
-    def customer_params
-      params.permit(Customer.attribute_names)
-    end
-end
