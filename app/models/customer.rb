168f6ea700400368654d8fb7cb7be45d03533cfd
Josi McClellan <josiahmcclellan07@gmail.com>
2018-01-25 14:59:04 -0700

    add logic for merchant's favorite customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,17 @@ class Customer < ApplicationRecord
 
   validates_presence_of :first_name, :last_name
 
+  def self.favorite_for_merchant(id)
+    joins(
+      invoices: :transactions
+    ).where(
+      invoices: { merchant_id: id },
+      transactions: { result: :success }
+    ).group(
+      :id
+    ).order(
+      'count(transactions.id) DESC'
+    ).first
+  end
+
 end
