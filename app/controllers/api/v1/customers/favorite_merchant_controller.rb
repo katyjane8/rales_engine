696c980a92c7e92112d85b4217a7a4ca079fb3c0
katyjane8 <katy.jane8@gmail.com>
2018-01-25 14:40:45 -0700

    small fix on previous commit - customer really passing now

--- app/controllers/api/v1/customers/favorite_merchant_controller.rb
+++ app/controllers/api/v1/customers/favorite_merchant_controller.rb
@@ -4,3 +4,4 @@ class Api::V1::Customers::FavoriteMerchantController < ApplicationController
     fav_merch = Customer.find(params[:customer_id])
     render json: fav_merch.favorite_merchant
   end
+end 
